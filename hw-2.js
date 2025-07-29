d6eb01a7034e14b1213884e2b6c95294e7acc33e
VNOksana <onv798@mail.ru>
2025-07-29 12:33:00 +0300

    моб версия +переменные

---
+++ hw-2.js
@@ -0,0 +1,50 @@
+
+//Задание 3
+let creatjs = "Brendan Eich";
+alert(creatjs);
+
+//Задание 4
+let a1 = 10;
+let b1 = 2;
+alert(a1 + b1);
+alert(a1 - b1);
+alert(a1 * b1);
+alert(a1 / b1);
+
+//Задание 5
+let result = 2 ** 5;
+alert(result);
+
+//Задание 6
+let a2 = 9;
+let b2 = 2;
+alert(a2 % b2);
+
+//Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+//Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+//Задание 9
+const user = {
+  name: "Oksana",
+  age: "35",
+  isAdmin: "true",
+};
+alert(user.name);
+alert(user.age);
+alert(user.isAdmin);
+
+//Задание 10
+let name1 = prompt("Как вас зовут?");
+alert(`Привет, ${name1}!`);
+
