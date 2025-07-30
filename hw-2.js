5dba5000467c5296b0e9cc892a7d833a65ccda52
VNOksana <onv798@mail.ru>
2025-07-30 18:19:37 +0300

    hw-2 переменные +вопросы js

--- hw-2.js
+++ hw-2.js
@@ -1,50 +1,74 @@
+// Task 1
+let a = 10
+alert(a)
+a = 20
+alert(a)
 
-//Задание 3
-let creatjs = "Brendan Eich";
-alert(creatjs);
+// Task2
+const iphone = 2007
+alert(iphone)
 
-//Задание 4
-let a1 = 10;
-let b1 = 2;
-alert(a1 + b1);
-alert(a1 - b1);
-alert(a1 * b1);
-alert(a1 / b1);
+// Task 3
+const nameJS = "BrendanEich"
+alert(nameJS)
 
-//Задание 5
-let result = 2 ** 5;
-alert(result);
+// Task 4
+let left = 10
+let right = 2
 
-//Задание 6
-let a2 = 9;
-let b2 = 2;
-alert(a2 % b2);
+alert(left + right)
+alert(left - right)
+alert(left * right)
+alert(left / right)
 
-//Задание 7
+// Task 5
+let result = 2 ** 5
+alert(result)
+
+// Task 6
+let z = 9
+let x = 2
+let divide = z % x
+alert(divide)
+
+// Task 7
 let num = 1;
 num += 5;
 num -= 3;
 num *= 7;
 num /= 3;
-num++;
-num--;
+num += 1;
+num -= 1;
 alert(num);
 
-//Задание 8
-let age = prompt("Сколько вам лет?");
-alert(age);
+// Task 8
+let age = prompt('Сколько вам лет?')
+alert(age)
 
-//Задание 9
+// Task 9
 const user = {
-  name: "Oksana",
-  age: "35",
-  isAdmin: "true",
-};
-alert(user.name);
-alert(user.age);
-alert(user.isAdmin);
+  name: 'Oksana',
+  age: 27,
+  isAdmin: true
+}
 
-//Задание 10
-let name1 = prompt("Как вас зовут?");
-alert(`Привет, ${name1}!`);
+// Task 10
+let question = prompt('Как вас зовут?')
+alert(`Привет ${question}!`)
+
+// Additional Task 
+let number = prompt('Загадайте число')
+numberTwo = number * 2
+alert(numberTwo)
+numberThree = numberTwo + 10
+alert(numberThree)
+numberFour = numberThree / 2
+alert(numberFour)
+numberFive = numberFour - number
+alert(numberFive)
+
+
+
+
+
 
---
+++ hw-3.js
@@ -0,0 +1,64 @@
+//
+// Вопрос 1 //
+let password = 'пароль';
+let askPassword = prompt('Введите пароль')
+
+if (askPassword === password) {
+    alert('Пароль введен верно');
+} else { alert('Пароль неверный') }
+
+
+// Вопрос 2 //
+let c = 10;
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+
+// Вопрос 3//
+let d = 64
+let e = 10
+
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно.');
+}
+
+
+// вопрос 4 //
+let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
+alert(Number(a) + Number(b));
+
+
+// вопрос 5 //
+let monthNumber = 12;
+switch (monthNumber) {
+    case 1:
+    case 2:
+    case 12:
+        console.log('Зима');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Весна');
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log('ЗЛето');
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log('Осень');
+        break;
+    default:
+        console.log('Неверно');
+        break;
+}
