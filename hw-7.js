7ce5e0ee76d0382dd8df3598237b2e8e5a1d5520
VNOksana <onv798@mail.ru>
2025-08-14 09:46:25 +0300

    Встроенные объекты

--- hw-7.js
+++ hw-7.js
@@ -5,6 +5,12 @@ let textAltered = text.toUpperCase();
 console.log(textAltered);
 
 //Task#2
+/**
+ * Создать функцию, которая принимает массив строк и строку. 
+ * Функция должна вернуть новый массив, содержащий только те элементы первого массива, 
+ * которые начинаются со второй строки. Регистр символов не влияет на результат.
+ */
+
 const searchElement = (elements, findIt) => {
     const newArray = new Array();
     elements.forEach(element => {
@@ -14,19 +20,31 @@ const searchElement = (elements, findIt) => {
     });
     return newArray;
 }
-console.log(searchElement(['Яблоки красные',  'ЯбЛОко Грин Делишес','Томаты', 'Яблочное пюре', 'яблоко Зеленое', 'Яблочный пирог', 'груша', 'Яблоко Голден'], 'яблоко'));
+console.log(searchElement(['Яблоки красные', 'ЯбЛОко Грин Делишес', 'Томаты', 'Яблочное пюре', 'яблоко Зеленое', 'Яблочный пирог', 'груша', 'Яблоко Голден'], 'яблоко'));
 
 
 //Task#3
 
+/**
+ * Округлить число 32.58884:
+
+До меньшего целого.
+До большего целого.
+До ближайшего целого.
+ */
+
 
 const num = 32.58884;
-    console.log(`До меньшего целого ${Math.floor(num)}`);
-    console.log(`До большего целого ${Math.ceil(num)}`);
-    console.log(`До ближайшего целого ${Math.round(num)}`);
-           
-         
+console.log(`До меньшего целого ${Math.floor(num)}`);
+console.log(`До большего целого ${Math.ceil(num)}`);
+console.log(`До ближайшего целого ${Math.round(num)}`);
+
+
 //Task#4
+/**
+ * Найти минимальное и максимальное значения из чисел 52, 53, 49, 77, 21, 32 
+ * и вывести их в консоль.
+ */
 
 
 const numbers = [52, 53, 49, 77, 21, 32];
@@ -36,7 +54,7 @@ for (let index = 0; index < numbers.length; index++) {
     if (numbers[index] > number) {
         number = numbers[index];
     }
-    
+
 }
 console.log(`Максимальное значение равно: ${number}`);
 
@@ -47,6 +65,9 @@ const toFindMin = () => {
 console.log(`Минимальное значение равно: ${toFindMin()}`);
 
 //Task#5
+/**
+ * Создать функцию, которая выводит в консоль случайное число от 1 до 10.
+ */
 
 const randomNumber = () => {
     return Math.floor(Math.random() * 10 + 1);
@@ -55,32 +76,44 @@ console.log(randomNumber());
 
 
 //Task#6
+/**
+ * Написать функцию, которая принимает целое число 
+ * и возвращает массив случайных чисел от 0 до этого числа. 
+ * Длина массива должна быть в два раза меньше переданного числа.
+ */
 
 function randomArray(n) {
     const newArray = [];
-    let randomNum; 
+    let randomNum;
     const newArrayLenth = n / 2;
     for (let i = 0; i < newArrayLenth; i++) {
         randomNum = Math.floor(Math.random() * n);
         newArray.push(randomNum);
-        
+
     }
     return newArray;
 }
 console.log(randomArray(6));
 
 //Task#7
+/**
+ * Создать функцию, которая принимает два целых числа
+ *  и возвращает случайное число в этом диапазоне.
+ */
 
 
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1) + min); 
-      }
-      console.log(getRandomInt(3, 8));
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+console.log(getRandomInt(3, 8));
 
 
 //Task#8
+/**
+ * Вывести в консоль текущую дату.
+ */
 
 let todaysDate = new Date();
 console.log(todaysDate);
@@ -89,12 +122,23 @@ console.log(todaysDate);
 //Task#9
 
 
+/**
+ * Создать переменную currentDate, хранящую текущую дату.
+ *  Вывести дату, которая наступит через 73 дня после текущей.
+ */
+
 let myDate = new Date();
 myDate.setDate(myDate.getDate() + 73);
 console.log(myDate);
 
 
 //Task#10
+/**
+ * Написать функцию, которая принимает дату и возвращает ее в формате:
+Дата: [число] [месяц на русском] [год] — это [день недели на русском].
+Время: [часы]:[минуты]:[секунды]
+ */
+
 
 const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 const days = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
